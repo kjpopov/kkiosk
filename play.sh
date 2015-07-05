e0c938812dd93274ca6541f876443a6bd5d424aa
Krasimir <kjpopov@mail.bg>
2015-07-05 10:52:59 +0000

    Mplayer stdin stdout stderr goes to devnull (We dont want our kiosk full of logs)

--- play.sh
+++ play.sh
@@ -3,5 +3,5 @@ export DISPLAY=:0.0
 
 CLIPFILE=`find /home/olimex/clip/ -type f | head -1`
 
-/usr/bin/mplayer "$CLIPFILE" -fs -loop 0
+/usr/bin/mplayer "$CLIPFILE" -fs -loop 0 -noconsolecontrols </dev/null >/dev/null 2>&1 &
 
